e4f51f53c0d41921b83be443ecd5110d79783bf2
trevor.patrick <40744678+trevor-patrick@users.noreply.github.com>
2018-10-19 21:09:17 -0400

    implemented book search feature

--- app.py
+++ app.py
@@ -30,13 +30,10 @@ def index():
         if check is None:
             message = "Incorrect username or password"
             return render_template("index.html", result = message)
-            # Alert user that credentials are incorrect
         else:
-            row = db.execute("SELECT id FROM accounts WHERE username = :username;", {"username": username}).fetchone()
-            session['id'] = row[0]
-            print(session['id'])
+            session["username"] = username
+            print(session["username"])
             return render_template("user_home.html", username = username)
-            # somehow sign the user in??
 
 
 @app.route("/createaccount", methods=['POST', 'GET'])
@@ -45,21 +42,61 @@ def create_acct():
         return render_template("create_acct.html")
 
     if request.method == "POST":
-        # implement feature to only allow alphanumeric usernames and password,
         # check if username already exists in DB
         username = request.form.get("Username")
         password = sha3.sha3_224(str(request.form.get("Password")).encode('utf-8')).hexdigest()
         check = db.execute("SELECT * FROM accounts WHERE username = :username", {"username": username}).fetchone()
-        # print(check)
+
         if check is None:
             db.execute("INSERT INTO accounts(username, password) VALUES(:username, :password)",
                 {"username": username, "password": password})
             db.commit()
-            # get the id of the current user logged in
-            row = db.execute("SELECT id FROM accounts WHERE username = :username;", {"username": username}).fetchone()
-            session['id'] = row[0]
-            print(session['id'])
-            # print(session["current_userID"])
+
+            # get the username of the current user logged in
+            session["username"] = username
+
             return render_template("user_home.html", username = username)
         else:
             return render_template("create_acct.html", username_taken = "Username already taken")
+
+
+@app.route("/search", methods=['POST'])
+def search():
+    results = []
+
+    if request.form.get("title") is not None:
+        title = request.form.get("title")
+    if request.form.get("author") is not None:
+        author = request.form.get("author")
+    if request.form.get("isbn") is not None:
+        isbn = request.form.get("isbn")
+
+
+    # --------------search by ISBN--------------
+    temp = db.execute("SELECT * FROM books where isbn = :isbn",
+                        {"isbn": isbn}).fetchall()
+    for item in temp:
+        if list(item) not in results:
+            results.append(list(item))
+
+
+    # --------------search by title--------------
+    temp = db.execute("SELECT * FROM books where title = :title",
+                        {"title": title}).fetchall()
+    for item in temp:
+        if list(item) not in results:
+            results.append(list(item))
+
+
+    # --------------search by author--------------
+    temp = db.execute("SELECT * FROM books where author = :author",
+                        {"author": author}).fetchall()
+    for item in temp:
+        if list(item) not in results:
+            results.append(list(item))
+
+
+    if not results:
+        return render_template("user_home.html", message = "No matches", username = session['username'])
+    else:
+        return render_template("results.html", results = session["username"])
